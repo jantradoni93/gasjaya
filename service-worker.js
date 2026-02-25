d79191bbad09e89e82a6b83a180ebefcbaa229a0
jantradoni93 <121495295+jantradoni93@users.noreply.github.com>
2026-02-26 06:48:24 +0800

    Update service worker cache version and assets

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,8 @@
-const CACHE_NAME = 'gas-jaya-cache-v1';
+const CACHE_NAME = 'gas-jaya-v2';
 const assets = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.4.1/papaparse.min.js'
 ];
 
